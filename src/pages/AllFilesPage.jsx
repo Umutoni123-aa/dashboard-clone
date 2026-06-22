8879d80f333ba760dc9abb3fb25ea654ef648c6a
Umutoni123-aa <u.nada@alustudent.com>
2026-06-22 16:40:56 +0200

    feat: add file manager page views

---
+++ src/pages/AllFilesPage.jsx
@@ -0,0 +1,3 @@
+export default function AllFilesPage() {
+  return <div className="p-6">All Files Page</div>
+}
---
+++ src/pages/TrashPage.jsx
@@ -0,0 +1,3 @@
+export default function TrashPage() {
+  return <div className="p-6">Trash Page</div>
+}
