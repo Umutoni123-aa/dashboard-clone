12c6afa3f1d60ee9b4904d933efab176eb9e4367
Umutoni123-aa <u.nada@alustudent.com>
2026-06-22 16:39:00 +0200

    chore: initialize React project and configure Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3A57E8",
+        info: "#08B1BA",
+        pageBg: "#F5F6FA",
+      },
+    },
+  },
+  plugins: [],
+};
